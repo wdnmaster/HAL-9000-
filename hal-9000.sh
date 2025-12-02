ee3beb74c626bcc5f86538079e3414905e642f5c
wdnmaster <welid.noureldin@gmail.com>
2025-12-02 00:40:46 +0000

    Adds hal program

---
+++ hal-9000.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+#A script to demo users input and variable use 
+
+echo " Hello. I'm HAL 9000.what is your name?"
+
+read USERNAME
+
+echo "i'm sorry $USERNAME. i'm afraid i can't open the pod  bay doors "
+
